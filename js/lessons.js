ac4216721dd5bcd66400b9b774e38c4265651d8f
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2024-07-22 10:40:21 +0200

    lessons file added

---
+++ js/lessons.js
@@ -0,0 +1,62 @@
+//document.getElementById("global");
+
+//var myGlobal = document.getElementById("global");
+
+/*let dreamHouse = {
+    bath: "Victorian",
+    garden: ["roses","cherry tree"],
+    piano: true,
+    pool: 200,
+
+
+};
+
+for(const i in dreamHouse) {
+    document.write(dreamHouse[i] + ", ");
+}*/
+
+//Objects
+
+//const exampleObjectKeys = { a: 1, b: 2, c: 3 };
+//console.log(Object.values(exampleObjectKeys) + Object.keys(exampleObjectKeys));
+
+/*const person = {
+    firstName: 'Ola',
+    lastName: 'Nordmann',
+    module: 'JavaScript'
+};
+
+const keysAndValues = Object.keys(person).map((key) => {
+
+    return`key: ${key}, value: ${person[key]}`;
+
+});
+
+console.log(keysAndValues);*/
+
+// Imports and exports
+
+//Traditional
+/*function add(a,b) {
+    return a + b;
+}*/
+
+
+//arrow
+
+//const add = (a,b) => a + b;
+
+
+//traditional
+
+let multiply = function(a,b) {
+    return a * b;
+};
+
+console.log(2*3);
+
+
+
+
+
+
