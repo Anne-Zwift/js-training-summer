966c61acd662900b56000f0bcab3f24f24f6b438
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2024-07-23 09:34:36 +0200

    lesson task added

---
+++ js/todo.js
@@ -0,0 +1,45 @@
+function createTodo(text) {
+    const todo = document.createElement("li");
+    todo.textContent = text;
+    todo.addEventListener("click", ontoggleTodo);
+    return todo;
+}
+
+function onNewTodo(event) {
+    event.preventDefault();
+    const { value } = event.target.todo;
+    const todo = createTodo(value);
+    document.querySelector("ul").append(todo);
+    event.target.todo.value = "";
+    event.target.todo.focus();
+}
+
+function ontoggleTodo(event) {
+    const { target } = event;
+    target.hidden = true;
+}
+
+document.forms.todo.addEventListener("submit", onNewTodo);
+
+/*function createTodo(text) {
+    const todo = document.createElement("li");
+    todo.textContent = text;
+    todo.addEventListener("click", onToggleTodo);
+    return todo;
+  }
+  
+  function onNewTodo(event) {
+    event.preventDefault();
+    const { value } = event.target.todo;
+    const todo = createTodo(value);
+    document.querySelector("ul").append(todo);
+    event.target.todo.value = "";
+    event.target.todo.focus();
+  }
+  
+  function onToggleTodo(event) {
+    const { target } = event;
+    target.hidden = true;
+  }
+  
+  document.forms.todo.addEventListener("submit", onNewTodo);*/
