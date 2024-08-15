b342fa1ff503394039ef2a070ce7303b709cae5f
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2024-08-15 11:08:03 +0200

    practicing json

--- js/storage.js
+++ js/storage.js
@@ -1,7 +1,41 @@
 const person = {
-    name: 'Ola Nordmann',
+    name: "Ola Nordmann",
     age: 30,
     isDeveloper: true
-  };
-  const jsonString = JSON.stringify(person);
-  // logs: '{"name":"Ola Nordmann","age":30,"isDeveloper":true}'
+};
+//const jsonString = JSON.stringify(person);
+
+//console.log(person);
+
+//Serialization - JSON.stringify()
+const book = {
+    title: "What I think of when i run",
+    author: "Hakuri Murakami",
+    publication: 1992
+};
+const jsonString = JSON.stringify(book);
+
+//console.log(book);
+//document.write(jsonString);
+
+
+//Deserialization - JSON.parse()
+/*const jsonString = '{"title": "What I think of when i run", "author": "Hakuri Murakami", "publication": 1992 }';
+const book = JSON.parse(jsonString);
+
+console.log(book);*/
+
+//Storing user name and theme preference in localStorage
+localStorage.setItem("user", "Ola Nordmann ");
+localStorage.setItem("theme", "dark");
+
+//Retrieving user name and theme preference from localStorage
+const user = localStorage.getItem("user");
+const theme = localStorage.getItem("theme");
+
+document.write(user, theme);
+console.log(user, theme);
+
+//Removing the theme preference from localStorage
+localStorage.removeItem("theme");
+
