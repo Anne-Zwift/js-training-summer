51365335a96521e72e1d8eb2514bf26cf5374d24
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2024-08-21 09:28:03 +0200

    exercise one and two

--- js/exercise1.js
+++ js/exercise1.js
@@ -34,11 +34,35 @@ console.log(myArray[2]);*/
 // log the variables so you know that you have destructured
 // them correctly.
 
-const values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+/*const values = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 const [firstValue, secondValue, , , fifthValue] = values;
-console.log(firstValue, secondValue, fifthValue);
+console.log(firstValue, secondValue, fifthValue);*/
 
 
+// Function to combine multiple arrays into one
+function combineArrays(...arrays) {
+    // The '...arrays' uses the Rest operator to collect all arguments into an array
+	// 'arrays' is now an array of arrays
+    //your code here
+
+    // We use the Spread operator '...' to expand each inner array
+	// Then, we use the Array.concat() method to combine them into a single array
+    return [].concat(...arrays);
+   
+}
 
 
+// Testing the function with different arrays
+
+console.log(combineArrays([1, 2], [3, 4], [5, 6]));
+// Expected output: [1, 2, 3, 4, 5, 6]
+
+// Additional test with different types of elements
+console.log(combineArrays(['Ola', 'Kari'], [true, false], [10, 20]));
+// Expected output: ['Ola', 'Kari', true, false, 10, 20]
+
+let myError = new Error("Something went wrong");
+
+console.log(myError.name);
+console.log(myError.message);
